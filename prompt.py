8c7a37aa9b59e3c8ff4e13b664ac14c3a189bd34
akuldevali <akulg005@gmail.com>
2025-06-15 02:04:52 -0400

    Trying out doc splitting,vector embeddings, similarity search

---
+++ prompt.py
@@ -0,0 +1,29 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+chat = ChatOpenAI()
+embeddings = OpenAIEmbeddings()
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings
+)
+
+'''this line basically makes sure that db object has a get_relevant_document method which is used by the RetrievalQA'''
+''' the as_retriever method of the chroma instance add the get_relev_doc method to itself'''
+'''This get_rele_doc method inturn calls the similarity_search method of the chroma instance'''
+'''All this is drama is to make sure that RetrievalQA is vector store agnostic'''
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm = chat,
+    retriever = retriever,
+    chain_type="stuff"
+)
+
+result = chain.invoke("What is an interesting fact about food?")
+
+print(result)
